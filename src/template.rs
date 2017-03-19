8f773a385bede99d6adf3cebf5aad6ddfaaf0ef4
dinesh <dineshpy07@gmail.com>
2017-03-19 20:08:59 +0530

    Add test case for class with methods

--- src/template.rs
+++ src/template.rs
@@ -6,63 +6,63 @@ use structures::{Class, Function};
 
 
 pub fn function_template(function: Function) -> String {
-	// return function template
-	let mut func_desc_bool = false;
-	let func_desc = match function.description {
-		Some(val) => {
-			func_desc_bool = true;
-			val
-		},
-		None => {
-			func_desc_bool = false;
-			String::new()
-		},
-	};
+    // return function template
+    let mut func_desc_bool = false;
+    let func_desc = match function.description {
+        Some(val) => {
+            func_desc_bool = true;
+            val
+        },
+        None => {
+            func_desc_bool = false;
+            String::new()
+        },
+    };
 
-	let function_template = r#"
+    let function_template = r#"
 def {{func_name}}({{parameters}}):
     {{#func_desc_bool}}"""
     {{func_desc}}
     """{{/func_desc_bool}}
     pass
-	"#;
+"#;
 
-	let mut function_parameters = String::new();
-	let mut i:i32 = 0;
+    let mut function_parameters = String::new();
+    let mut i:i32 = 0;
 
-	for parameter in function.parameters {
-		if i != 0 {
-			function_parameters += ", "
-		}
-		function_parameters += &parameter;
-		i += 1;
-	}
+    for parameter in function.parameters {
+        if i != 0 {
+            function_parameters += ", "
+        }
+        function_parameters += &parameter;
+        i += 1;
+    }
 
-	let mut data = HashBuilder::new();
-	data = data.insert("parameters", function_parameters);
-	data = data.insert("func_name", function.name);
+    let mut data = HashBuilder::new();
+    data = data.insert("parameters", function_parameters);
+    data = data.insert("func_name", function.name);
 
-	data = data.insert("func_desc", func_desc);
-	data = data.insert("func_desc_bool", func_desc_bool);
+    data = data.insert("func_desc", func_desc);
+    data = data.insert("func_desc_bool", func_desc_bool);
 
-	let mut out = Cursor::new(Vec::new());
-	data.render(function_template, &mut out);
+    let mut out = Cursor::new(Vec::new());
+    data.render(function_template, &mut out);
 
-	// return the filled template.
-	// TODO: Handle error's
-	String::from_utf8(out.into_inner()).unwrap()
+    // return the filled template.
+    // TODO: Handle error's
+    String::from_utf8(out.into_inner()).unwrap()
 }
 
 pub fn method_template(method: Function) -> String {
     let method_template = r#"
-	def {{ method_name }}(self, ):
-		{{#method_desc_bool}}"""
-		{{ method_desc }}
-		"""{{/method_desc_bool}}
+    def {{ method_name }}(self, ):
+        {{#method_desc_bool}}"""
+        {{ method_desc }}
+        """{{/method_desc_bool}}
         pass
-	"#;
+"#;
 
-	let mut method_template_string = String::new();
+    let mut method_template_string = String::new();
 
     let method_desc_bool = false;
 
@@ -83,65 +83,65 @@ pub fn method_template(method: Function) -> String {
 }
 
 pub fn class_template(class: Class) -> String {
-	let mut class_desc_bool = false;
-	let class_desc = match class.description {
-		Some(val) => {
-			class_desc_bool = true;
-			val
-		},
-		None => {
-			class_desc_bool = false;
-			String::new()
-		}
-	};
+    let mut class_desc_bool = false;
+    let class_desc = match class.description {
+        Some(val) => {
+            class_desc_bool = true;
+            val
+        },
+        None => {
+            class_desc_bool = false;
+            String::new()
+        }
+    };
 
-	let class_template = r#"
+    let class_template = r#"
 class {{ class_name }}:
     {{#class_desc_bool}}"""
     {{ class_desc }}
     """{{/class_desc_bool}}
     def __init__(self):
         pass
-    "#;
+"#;
 
-	let mut method_template_string = String::new();
-	let methods = class.methods;
+    let mut method_template_string = String::new();
+    let methods = class.methods;
 
-	for method in methods {
-		method_template_string += &method_template(method);
-	}
+    for method in methods {
+        method_template_string += &method_template(method);
+    }
 
-	let mut data = HashBuilder::new();
-	data = data.insert("class_name", class.name);
-	data = data.insert("class_desc", class_desc);
-	data = data.insert("class_desc_bool", class_desc_bool);
+    let mut data = HashBuilder::new();
+    data = data.insert("class_name", class.name);
+    data = data.insert("class_desc", class_desc);
+    data = data.insert("class_desc_bool", class_desc_bool);
 
-	let mut out = Cursor::new(Vec::new());
-	data.render(class_template, &mut out);
+    let mut out = Cursor::new(Vec::new());
+    data.render(class_template, &mut out);
 
-	// return the filled class template
-	String::from_utf8(out.into_inner()).unwrap() + &method_template_string
+    // return the filled class template
+    String::from_utf8(out.into_inner()).unwrap() + &method_template_string
 }
 
 // Unit tests.
 #[test]
 fn test_function_template() {
-	let function = Function {
-		name: "display".to_string(),
-		description: Some("This is the display function.".to_string()),
-		parameters: Vec::new()
-	};
+    let function = Function {
+        name: "display".to_string(),
+        description: Some("This is the display function.".to_string()),
+        parameters: Vec::new()
+    };
 
-	let function_template_content = function_template(function);
-	let expected_function_template_content = r#"
+    let function_template_content = function_template(function);
+    let expected_function_template_content = r#"
 def display():
     """
     This is the display function.
     """
     pass
-	"#;
+"#;
 
-	assert_eq!(function_template_content, expected_function_template_content);
+    assert_eq!(function_template_content, expected_function_template_content);
 }
 
 #[test]
@@ -160,7 +160,45 @@ class Animal:
     """
     def __init__(self):
         pass
-    "#;
+"#;
+
+    assert_eq!(class_template_content, expected_class_template_content);
+}
+
+#[test]
+fn test_class_template_with_methods() {
+    let mut methods = Vec::new();
+
+    let function = Function {
+        name: "display".to_string(),
+        description: Some("This is the display function.".to_string()),
+        parameters: Vec::new()
+    };
+
+    methods.push(function);
+
+    let class = Class {
+        name: "Animal".to_string(),
+        description: Some("This is the animal class.".to_string()),
+        methods: methods
+    };
+
+    let class_template_content = class_template(class);
+
+    let expected_class_template_content = r#"
+class Animal:
+    """
+    This is the animal class.
+    """
+    def __init__(self):
+        pass
+
+    def display(self, ):
+        """
+        This is the display function.
+        """
+        pass
+"#;
 
     assert_eq!(class_template_content, expected_class_template_content);
 }
