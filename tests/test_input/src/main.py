ef8853a4ad2cb9c957683644a2253d01b8135f11
dinesh <dineshpy07@gmail.com>
2017-05-12 23:44:14 +0530

    Remove .py from .gitignore

---
+++ tests/test_input/src/main.py
@@ -0,0 +1,12 @@
+"""
+This is the main module.
+"""
+import os
+
+print "Current directory is", os.getcwd()
+
+def main():
+    """
+    Main function
+    """
+    print "This is the main function"
